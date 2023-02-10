614a4127492bc5319f00e3d4388179f32527b1e3
Camila25012002 <cquinteroorozco@unicauca.edu.co>
2023-02-10 15:11:09 -0500

    Proyecto Final Aplimovil

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDT0W3FbMxmPMB5VeAvAHQ0CvkErV8bjPc",
+    authDomain: "trabajofinalionic.firebaseapp.com",
+    projectId: "trabajofinalionic",
+    storageBucket: "trabajofinalionic.appspot.com",
+    messagingSenderId: "397275164081",
+    appId: "1:397275164081:web:e1374a88d905cb6551e190"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDT0W3FbMxmPMB5VeAvAHQ0CvkErV8bjPc",
+    authDomain: "trabajofinalionic.firebaseapp.com",
+    projectId: "trabajofinalionic",
+    storageBucket: "trabajofinalionic.appspot.com",
+    messagingSenderId: "397275164081",
+    appId: "1:397275164081:web:e1374a88d905cb6551e190"
+  }
 };
 
 /*
